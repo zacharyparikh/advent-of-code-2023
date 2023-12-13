71099efb98de684d8e96f336a23e5b3c0bbd8f13
zparikh <zparikh@paypal.com>
2023-12-13 13:34:10 -0500

    Day 13 Part 2

--- solutions/day13.ts
+++ solutions/day13.ts
@@ -1,44 +1,58 @@
 import { range } from "../util/range.ts";
 
-type Pattern = { rows: string[], columns: string[] }
+type Pattern = { rows: number[]; columns: number[] };
 
-function findReflection({rows, columns}: Pattern) {
-  const find = (ps: string[]) => range(1, ps.length).find((index) => {
-    let left = index - 1;
-    let right = index;
-    
-    while (left >= 0 && right < ps.length) {
-      if (ps[left] !== ps[right]) {
-        return false;
-      }
-      left -= 1;
-      right += 1;
+const countBitDifference = (n1: number, n2: number) => {
+  let xor = n1 ^ n2;
+  let count = 0;
+  while (xor) {
+    count += xor & 1;
+    xor >>= 1;
+  }
+  return count;
+};
+
+const findReflection =
+  (numSmudges: number) =>
+  ({ rows, columns }: Pattern) => {
+    const find = (ps: number[]) =>
+      range(1, ps.length).find((index) => {
+        let left = index - 1;
+        let right = index;
+        let bitDifference = 0;
+
+        while (left >= 0 && right < ps.length && bitDifference <= numSmudges) {
+          bitDifference += countBitDifference(ps[left], ps[right]);
+          left -= 1;
+          right += 1;
+        }
+
+        return bitDifference === numSmudges;
+      });
+
+    const rowReflection = find(rows);
+
+    if (rowReflection) {
+      return 100 * rowReflection;
     }
 
-    return true;
-  })
-
-  const rowReflection = find(rows);
-
-  if (rowReflection) {
-    return 100 * rowReflection;
-  }
-
-  const columnReflection = find(columns);
-  return columnReflection ?? 0;
-}
+    const columnReflection = find(columns);
+    return columnReflection ?? 0;
+  };
 
 function part1(patterns: Pattern[]) {
-  const reflections = patterns.map(findReflection);
-  console.log({reflections});
-  
-  return reflections.reduce((a, b) => a + b)
-  
+  const reflections = patterns.map(findReflection(0));
+  return reflections.reduce((a, b) => a + b);
 }
 
-function part2() {}
+function part2(patterns: Pattern[]) {
+  const reflections = patterns.map(findReflection(1));
+  return reflections.reduce((a, b) => a + b);
+}
 
 export function solve() {
+  const patternToNumber = (p: string) =>
+    parseInt(Array.from(p, (x) => (x === "#" ? "1" : "0")).join(""), 2);
   const patterns = Deno.readTextFileSync("inputs/day13.txt")
     .split("\n\n")
     .map((pattern) => {
@@ -49,10 +63,12 @@ export function solve() {
           columns[i] = (columns[i] ?? "") + x;
         })
       );
-      return { rows, columns };
+
+      const rowValues = rows.map(patternToNumber);
+      const columnValues = columns.map(patternToNumber);
+
+      return { rows: rowValues, columns: columnValues };
     });
 
-  console.log({ patterns });
-
-  return `Part 1: ${part1(patterns)}\nPart 2: ${part2()}`;
+  return `Part 1: ${part1(patterns)}\nPart 2: ${part2(patterns)}`;
 }
